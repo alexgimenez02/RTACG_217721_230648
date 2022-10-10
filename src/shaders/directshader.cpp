51a9b6b750a5913da0e56f5a913264e2919884ab
Àlex Giménez <alex.gimenez02@estudiant.upf.edu>
2022-10-10 21:44:32 +0200

    Corrected a little mistake

--- src/shaders/directshader.cpp
+++ src/shaders/directshader.cpp
@@ -18,7 +18,7 @@ Vector3D DirectShader::computeColor(const Ray& r, const std::vector<Shape*>& obj
             
             Vector3D Li = lsList.at(i).getIntensity(its->itsPoint);
             Vector3D wi = (lsList.at(i).getPosition() - its->itsPoint).normalized();
-            double maxT = wi.length();
+            double maxT = (lsList.at(i).getPosition() - its->itsPoint).length();
 
             Ray wiRay = Ray(its->itsPoint, wi, 0, Epsilon, maxT);
             Vector3D rp = its->shape->getMaterial().getReflectance(its->normal, (r.o - its->itsPoint).normalized(), wi);
