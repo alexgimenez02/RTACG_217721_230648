20bec69fabac82d635b709a77582e4c5668d1709
Àlex Giménez Saludes <alex.gimenez02@estudiant.upf.edu>
2022-10-19 19:26:44 +0200

    Espejo arreglado
    Co-Authored-By: Sofía Cárdenas <65339757+SofiaCardenasLinares@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -115,7 +115,9 @@ void buildSceneCornellBox(Camera*& cam, Film*& film,
     Material* greyDiffuse = new Phong(Vector3D(0.8, 0.8, 0.8), Vector3D(0, 0, 0), 100);
     Material* blueDiffuse = new Phong(Vector3D(0.3, 0.2, 0.7), Vector3D(0, 0, 0), 100);
     Material* transmissive = new Transmissive(1.1);
+    //Material* transmissive = new Phong(Vector3D(1,1,0.2), Vector3D(1,1,0.2),20);
     Material* mirror = new Mirror();
+    //Material* mirror = new Phong(Vector3D(0.0, 0.9, 0.9), Vector3D(0.1, 0.9, 0.9), 50);
     Material* red_100 = new Phong(Vector3D(0.7, 0.2, 0.3), Vector3D(0.7, 0.7, 0.2), 100);
 
     /* ******* */
@@ -244,7 +246,7 @@ int main()
 
     // Declare the shader
     Vector3D bgColor(0.0, 0.0, 0.0); // Background color (for rays which do not intersect anything)
-    Vector3D intersectionColor(0.15, 0.15, 0.15);
+    Vector3D intersectionColor(0.5, 0.5, 0.5);
     
     //Shader* shader = new IntersectionShader(intersectionColor, bgColor); //task 2
     //Shader* shader = new DepthIntersectionShader(intersectionColor, bgColor); //task 3
--- src/shaders/globalshader.cpp
+++ src/shaders/globalshader.cpp
@@ -11,68 +11,61 @@ GlobalShader::GlobalShader(Vector3D at, Vector3D bgColor_){
 Vector3D GlobalShader::computeColor(const Ray& r, const vector<Shape*>& objList, const vector<PointLightSource>& lsList) const{
     Intersection* its = new Intersection();
     if (Utils::getClosestIntersection(r, objList, *its)) {
-        if (r.depth == 0) {
-            if(its->shape->getMaterial().hasDiffuseOrGlossy()){
-                Vector3D Lind = Vector3D(0);
-                Lind = this->at * its->shape->getMaterial().getDiffuseCoefficient();
+        
+        if(its->shape->getMaterial().hasDiffuseOrGlossy()){
+            Vector3D Lind = Vector3D(0);
+            Lind = this->at * its->shape->getMaterial().getDiffuseCoefficient();
 
-                Vector3D Lo = Vector3D(0, 0, 0);
-                for (int i = 0; i < lsList.size(); i++) {
+            Vector3D Lo = Vector3D(0, 0, 0);
+            for (int i = 0; i < lsList.size(); i++) {
 
-                    Vector3D Li = lsList.at(i).getIntensity(its->itsPoint);
-                    Vector3D wi = (lsList.at(i).getPosition() - its->itsPoint).normalized();
-                    double maxT = (lsList.at(i).getPosition() - its->itsPoint).length();
+                Vector3D Li = lsList.at(i).getIntensity(its->itsPoint);
+                Vector3D wi = (lsList.at(i).getPosition() - its->itsPoint).normalized();
+                double maxT = (lsList.at(i).getPosition() - its->itsPoint).length();
 
-                    Ray wiRay = Ray(its->itsPoint, wi, 0, Epsilon, maxT);
-                    Vector3D rp = its->shape->getMaterial().getReflectance(its->normal, (r.o - its->itsPoint).normalized(), wi);
+                Ray wiRay = Ray(its->itsPoint, wi, 0, Epsilon, maxT);
+                Vector3D rp = its->shape->getMaterial().getReflectance(its->normal, (r.o - its->itsPoint).normalized(), wi);
 
-                    if (!Utils::hasIntersection(wiRay, objList)) {
-                        Lo += Li * rp;
-                    }
-                }
-                return Lo + Lind;
-            }else if(its->shape->getMaterial().hasSpecular()){
-                // PSEUDO-CODE
-                // Vector3D wr = computeReflectionDirection(ray.d,its->normal)
-                /* Ray reflectionRay = Ray(its->point,wr,ray.depth+1)
-                 * color = ComputeColor(reflectionRay,objLit,lsList)
-                 * return color;
-                 */
-                Vector3D wr = cross(r.d, its->normal);
-                //Vector3D wr = Vector3D(0);
-                Ray reflectionRay = Ray(its->itsPoint, wr, r.depth + 1);
-                return computeColor(reflectionRay, objList, lsList);
-                
-            }else if(its->shape->getMaterial().hasTransmission()){
-                /* PSEUDO-CODE
-                 * if !isTotalInternalReflection(...){
-                 *     wt = computeTransmissionDirection(...)
-                 *     refracRay = Ray(its->point,wt,ray.depth+1)
-                 *     color = ComputeColor(refracRay,objList,lsList)
-                 * } else{
-                 *     color = Specular reflection
-                 * }
-                 * return color;
-                 */
-                double insRot = (1 - pow(its->shape->getMaterial().getIndexOfRefraction(), 2) * (1 - pow(dot(its->itsPoint - r.o, its->normal), 2)));
-                if (insRot >= 0) {
-                    //Calcular wt
-                    double t = sqrt(insRot) - its->shape->getMaterial().getIndexOfRefraction() * dot(its->itsPoint - r.o, its->normal);
-                    Vector3D wt = its->normal * t + (its->itsPoint - r.o) * its->shape->getMaterial().getIndexOfRefraction();
-                    //Vector3D wt = Vector3D(0);
-                    Ray refractedRay = Ray(its->itsPoint, wt, r.depth + 1);
-                    return computeColor(refractedRay, objList, lsList);
-                }
-                else {
-                    Vector3D wr = cross(r.d, its->normal);
-                    Ray reflectionRay = Ray(its->itsPoint, wr, r.depth + 1);
-                    return computeColor(reflectionRay, objList, lsList);
+                if (!Utils::hasIntersection(wiRay, objList)) {
+                    Lo += Li * rp;
                 }
             }
-        }
-        else {
+            return Lo + Lind;
+        }else if(its->shape->getMaterial().hasSpecular()){
+            // PSEUDO-CODE
+            // Vector3D wr = computeReflectionDirection(ray.d,its->normal)
+            /* Ray reflectionRay = Ray(its->point,wr,ray.depth+1)
+                * color = ComputeColor(reflectionRay,objLit,lsList)
+                * return color;
+                */
+            Vector3D wo = r.o - its->itsPoint;
+            Vector3D wr = its->normal * 2 * dot(wo.normalized(), its->normal) - wo.normalized();
+            Ray reflectionRay = Ray(its->itsPoint, wr, r.depth + 1);
+            return computeColor(reflectionRay, objList, lsList);
 
+                
+        }else if(its->shape->getMaterial().hasTransmission()){
+            /* PSEUDO-CODE
+                * if !isTotalInternalReflection(...){
+                *     wt = computeTransmissionDirection(...)
+                *     refracRay = Ray(its->point,wt,ray.depth+1)
+                *     color = ComputeColor(refracRay,objList,lsList)
+                * } else{
+                *     color = Specular reflection
+                * }
+                * return color;
+                */
+            /*Vector3D wo = r.o - its->itsPoint;
+            double etaT = 1 / its->shape->getMaterial().getIndexOfRefraction();
+            double insRot = 1 - (pow(etaT, 2) * (1 - pow(dot(wo.normalized(), its->normal), 2)));
+            if (insRot >= 0){
+                double outRot = -sqrt(insRot) + etaT * dot(wo.normalized(), its->normal);
+                Vector3D wt = its->normal * outRot - wo.normalized() * etaT;
+                Ray refractedRay = Ray(its->itsPoint, wt, r.depth + 1);
+                return computeColor(refractedRay, objList, lsList);
+            }*/
         }
+        
     }
     else
         return bgColor;
